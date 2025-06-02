e2bcd3d009e0f8b15eb4409609148a30b2c26a1d
Fermenade <93608551+Fermenade@users.noreply.github.com>
2025-06-02 05:53:32 +0200

    updated database

--- Dokumentation/deliveryservice.sql
+++ Dokumentation/deliveryservice.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 02, 2025 at 05:41 AM
+-- Generation Time: Jun 02, 2025 at 05:52 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -20,6 +20,107 @@ SET time_zone = "+00:00";
 --
 -- Database: `deliveryservice`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `address`
+--
+
+CREATE TABLE `address` (
+  `addressID` int(11) NOT NULL,
+  `country` varchar(80) NOT NULL,
+  `postzip` int(11) NOT NULL,
+  `city` varchar(45) NOT NULL,
+  `street` varchar(45) NOT NULL,
+  `housenr` varchar(10) NOT NULL,
+  `apartmentnr` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `address`
+--
+
+INSERT INTO `address` (`addressID`, `country`, `postzip`, `city`, `street`, `housenr`, `apartmentnr`) VALUES
+(1, 'Deutschesland', 2222, 'Deutsch', 'Land', 'Haus', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_has_product`
+--
+
+CREATE TABLE `order_has_product` (
+  `orderID` int(11) NOT NULL,
+  `user_userID` int(11) NOT NULL,
+  `product_productD` int(11) NOT NULL,
+  `amount` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `productID` int(11) NOT NULL,
+  `name` varchar(80) NOT NULL,
+  `productdescription` text DEFAULT NULL,
+  `price` decimal(10,0) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `product`
+--
+
+INSERT INTO `product` (`productID`, `name`, `productdescription`, `price`) VALUES
+(1, 'Leaky soup', 'Suppe aber leaky', 6),
+(2, 'Leaky House Soup', 'Wie Leaky Suppe, aber Haus.', 5),
+(3, 'Leaky Leaky Soup', 'Leaky Suppe die leaky ist.', 23),
+(4, 'Soup Soup Soup', 'Soup Soup Soup', 12);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `productgroup`
+--
+
+CREATE TABLE `productgroup` (
+  `productgroupID` int(11) NOT NULL,
+  `name` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `productgroup`
+--
+
+INSERT INTO `productgroup` (`productgroupID`, `name`) VALUES
+(1, 'Vorspeise'),
+(2, 'Hauptspeise'),
+(3, 'Nachspeise'),
+(4, 'Subbe');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_has_productgroup`
+--
+
+CREATE TABLE `product_has_productgroup` (
+  `productID` int(11) NOT NULL,
+  `productgroupID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `product_has_productgroup`
+--
+
+INSERT INTO `product_has_productgroup` (`productID`, `productgroupID`) VALUES
+(1, 4),
+(2, 4),
+(3, 4),
+(4, 4);
 
 -- --------------------------------------------------------
 
@@ -42,9 +143,64 @@ CREATE TABLE `user` (
 INSERT INTO `user` (`userID`, `usertypeID`, `email`, `password`, `address_addressID`) VALUES
 (9, 1, 'Deutsch@Deutschmann', 'eristpole', 1);
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usertype`
+--
+
+CREATE TABLE `usertype` (
+  `usertypeID` int(11) NOT NULL,
+  `name` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `usertype`
+--
+
+INSERT INTO `usertype` (`usertypeID`, `name`) VALUES
+(1, 'Konsument'),
+(2, 'Mitarbeiter'),
+(3, 'Administrator');
+
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `address`
+--
+ALTER TABLE `address`
+  ADD PRIMARY KEY (`addressID`);
+
+--
+-- Indexes for table `order_has_product`
+--
+ALTER TABLE `order_has_product`
+  ADD PRIMARY KEY (`orderID`,`user_userID`,`product_productD`),
+  ADD KEY `fk_Produkt_has_Bestellung_Produkt1_idx` (`product_productD`),
+  ADD KEY `fk_Order_has_Product_User1_idx` (`user_userID`);
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`productID`),
+  ADD UNIQUE KEY `ProduktID_UNIQUE` (`productID`);
+
+--
+-- Indexes for table `productgroup`
+--
+ALTER TABLE `productgroup`
+  ADD PRIMARY KEY (`productgroupID`);
+
+--
+-- Indexes for table `product_has_productgroup`
+--
+ALTER TABLE `product_has_productgroup`
+  ADD PRIMARY KEY (`productID`,`productgroupID`),
+  ADD KEY `fk_Produkt_has_Produktgruppe_Produktgruppe1_idx` (`productgroupID`),
+  ADD KEY `fk_Produkt_has_Produktgruppe_Produkt1_idx` (`productID`);
 
 --
 -- Indexes for table `user`
@@ -56,8 +212,33 @@ ALTER TABLE `user`
   ADD KEY `fk_User_Address1_idx` (`address_addressID`);
 
 --
+-- Indexes for table `usertype`
+--
+ALTER TABLE `usertype`
+  ADD PRIMARY KEY (`usertypeID`),
+  ADD UNIQUE KEY `usertypeID_UNIQUE` (`usertypeID`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `address`
+--
+ALTER TABLE `address`
+  MODIFY `addressID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `productID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `productgroup`
+--
+ALTER TABLE `productgroup`
+  MODIFY `productgroupID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `user`
@@ -66,8 +247,28 @@ ALTER TABLE `user`
   MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
+-- AUTO_INCREMENT for table `usertype`
+--
+ALTER TABLE `usertype`
+  MODIFY `usertypeID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `order_has_product`
+--
+ALTER TABLE `order_has_product`
+  ADD CONSTRAINT `fk_Order_has_Product_User1` FOREIGN KEY (`user_userID`) REFERENCES `user` (`userID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_Produkt_has_Bestellung_Produkt1` FOREIGN KEY (`product_productD`) REFERENCES `product` (`productID`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `product_has_productgroup`
+--
+ALTER TABLE `product_has_productgroup`
+  ADD CONSTRAINT `fk_Produkt_has_Produktgruppe_Produkt1` FOREIGN KEY (`productID`) REFERENCES `product` (`productID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_Produkt_has_Produktgruppe_Produktgruppe1` FOREIGN KEY (`productgroupID`) REFERENCES `productgroup` (`productgroupID`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Constraints for table `user`
