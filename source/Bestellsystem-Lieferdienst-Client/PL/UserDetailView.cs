dfa294f9bcdcbba59c658ed4d429994d3ccc6b34
snipz8181 <melikalper1@gmail.com>
2025-06-11 12:44:40 +0200

    änderung

--- source/Bestellsystem-Lieferdienst-Client/PL/UserDetailView.cs
+++ source/Bestellsystem-Lieferdienst-Client/PL/UserDetailView.cs
@@ -7,6 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Bestellsystem_Lieferdienst_Client.BL;
+using Bestellsystem_Lieferdienst_Client.PL;
+using Bestellsystem_Lieferdienst_Client.Server;
+using Client_Server_Code_Library;
 
 namespace Bestellsystem_Lieferdienst.PL
 {
@@ -14,7 +18,37 @@ namespace Bestellsystem_Lieferdienst.PL
     {
         public UserDetailView()
         {
+            if (Client.client.User == null)
+            {
+                MessageBox.Show("Sie sind nicht eingeloggt!");
+                return;
+            }
+
             InitializeComponent();
+            InitializeManuqlComponent();
+
+
+        }
+
+        void InitializeManuqlComponent()
+        {
+            tbxEMail.Text = Client.client.User.email.ToString();
+            tbxStraße.Text = Client.client.User.Address.Street.ToString();
+            tbxHausnummer.Text = Client.client.User.Address.HouseNumber.ToString();
+            tbxApartment.Text = Client.client.User.Address.ApartmentNumber.ToString();
+            tbxPLZ.Text = Client.client.User.Address.ZippCode.ToString();
+            tbxOrt.Text = Client.client.User.Address.City.ToString();
+            tbxLand.Text = Client.client.User.Address.Country.ToString();
+        }
+
+        private void btnChangePassword_Click(object sender, EventArgs e)
+        {
+            if (TryCreateUser(tbxEMail.Text,tbxPassword.Text,tbxLand.Text,tbxPLZ.Text,tbxOrt.Text,tbxStraße.Text,tbxHausnummer.Text,tbxApartment.Text, out User user))
+            {
+                GetData.SetUser(user);
+            }
+
+
         }
     }
 }
