cc21d660422a52e77534b5180cde370223c6fc13
Fermenade <93608551+Fermenade@users.noreply.github.com>
2025-06-19 03:28:24 +0200

    removed 1 label

--- source/Bestellsystem-Lieferdienst-Client/PL/Consumer/ConnectionLost.Designer.cs
+++ source/Bestellsystem-Lieferdienst-Client/PL/Consumer/ConnectionLost.Designer.cs
@@ -36,15 +36,15 @@ namespace Bestellsystem_Lieferdienst_Client.PL
             label2 = new Label();
             label3 = new Label();
             numericUpDown1 = new NumericUpDown();
-            label4 = new Label();
             ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(522, 607);
+            button1.Location = new Point(307, 370);
+            button1.Margin = new Padding(2);
             button1.Name = "button1";
-            button1.Size = new Size(188, 58);
+            button1.Size = new Size(111, 35);
             button1.TabIndex = 0;
             button1.Text = "Connect";
             button1.UseVisualStyleBackColor = true;
@@ -53,65 +53,62 @@ namespace Bestellsystem_Lieferdienst_Client.PL
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(467, 160);
+            label1.Location = new Point(275, 98);
+            label1.Margin = new Padding(2, 0, 2, 0);
             label1.Name = "label1";
-            label1.Size = new Size(738, 41);
+            label1.Size = new Size(438, 25);
             label1.TabIndex = 1;
             label1.Text = "Verbindung zu Server konnte nicht hergestellt werden.";
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(535, 291);
+            textBox2.Location = new Point(315, 177);
+            textBox2.Margin = new Padding(2);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(250, 47);
+            textBox2.Size = new Size(149, 31);
             textBox2.TabIndex = 3;
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(558, 230);
+            label2.Location = new Point(328, 140);
+            label2.Margin = new Padding(2, 0, 2, 0);
             label2.Name = "label2";
-            label2.Size = new Size(160, 41);
+            label2.Size = new Size(97, 25);
             label2.TabIndex = 4;
             label2.Text = "IP-Adresse";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(535, 408);
+            label3.Location = new Point(315, 249);
+            label3.Margin = new Padding(2, 0, 2, 0);
             label3.Name = "label3";
-            label3.Size = new Size(72, 41);
+            label3.Size = new Size(44, 25);
             label3.TabIndex = 5;
             label3.Text = "Port";
             // 
             // numericUpDown1
             // 
-            numericUpDown1.Location = new Point(535, 465);
+            numericUpDown1.Location = new Point(315, 284);
+            numericUpDown1.Margin = new Padding(2);
             numericUpDown1.Name = "numericUpDown1";
-            numericUpDown1.Size = new Size(300, 47);
+            numericUpDown1.Size = new Size(176, 31);
             numericUpDown1.TabIndex = 6;
-            // 
-            // label4
-            // 
-            label4.AutoSize = true;
-            label4.Location = new Point(488, 559);
-            label4.Name = "label4";
-            label4.Size = new Size(0, 41);
-            label4.TabIndex = 7;
             // 
             // ConnectionLost
             // 
-            AutoScaleDimensions = new SizeF(17F, 41F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            Controls.Add(label4);
             Controls.Add(numericUpDown1);
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(textBox2);
             Controls.Add(label1);
             Controls.Add(button1);
+            Margin = new Padding(2);
             Name = "ConnectionLost";
-            Size = new Size(1540, 763);
+            Size = new Size(906, 465);
             ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
             ResumeLayout(false);
             PerformLayout();
@@ -132,6 +129,5 @@ namespace Bestellsystem_Lieferdienst_Client.PL
         private Label label2;
         private Label label3;
         private NumericUpDown numericUpDown1;
-        private Label label4;
     }
 }
